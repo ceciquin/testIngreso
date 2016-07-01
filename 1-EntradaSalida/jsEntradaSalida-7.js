1fba1754fd58fb6d3bc56fab24110f3b57bec768
ceciquin <ceciquin@protonmail.ch>
2016-07-01 20:09:23 -0300

    mi segundo commit

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,7 +3,7 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{	alert("sumar")
 	
 }
 
