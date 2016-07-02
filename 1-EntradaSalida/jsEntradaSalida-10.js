797b43fce754f68771290c5cf67e719d8c0298a7
ceciquin <ceciquin@protonmail.ch>
2016-07-01 22:07:42 -0300

    cuarto commit

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,9 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
-{
+{ var importe= document.getElementById('importe').value;
+var resultado= document.getElementById('resultado').value;
+resultado= parseInt(importe)*parseInt(resultado);
+
 	
 }
